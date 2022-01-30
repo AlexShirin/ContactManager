f32727e14986a4217b31ba8a03891d501a20fcf6
AlexShirin <aldorian@mail.ru>
2022-01-30 12:13:41 +0300

    added integration tests

--- src/test/java/com/example/ContactManager/ContactManagerIntegrationTests.java
+++ src/test/java/com/example/ContactManager/ContactManagerIntegrationTests.java
@@ -2,98 +2,226 @@ package com.example.ContactManager;
 
 import com.example.ContactManager.Controller.ContactController;
 import com.example.ContactManager.Model.Contact;
-import com.example.ContactManager.Repository.ContactRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.springframework.http.HttpHeaders.*;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 
-//@ExtendWith(SpringExtension.class)
-//@ExtendWith(MockitoExtension.class)
-//@SpringBootTest(classes = ContactManagerApplication.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ContactManagerIntegrationTests {
+    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
 
     @Autowired
-    private ObjectMapper objectMapper;
+    WebTestClient webTestClient;
+    @Autowired
+    ObjectMapper objectMapper;
 
-    WebTestClient webTestClient = WebTestClient.bindToController(ContactController.class).build();
+    @BeforeEach
+    void setUp() {
+    }
 
-
-    @Disabled
     @Test
-    public void testAllContacts() throws JsonProcessingException {
+    public void GetAllContactsTest() throws JsonProcessingException {
+        EntityExchangeResult<List<Contact>> response = webTestClient
+                .get()
+                .uri("/contact") // the base URL is already configured for us
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body = response.getResponseBody();
+        log.info("* testGetAllContacts, List<Contact>=\n{}", body);
+        assertThat(body).isNotNull();
+        assertThat(body.size()).isEqualTo(5);
+    }
+
+    @Test
+    void GetAllContactsWithParamTest() {
+//        String uri = UriComponentsBuilder
+//                .fromUriString("/contact")
+//                .queryParam("page", "0")
+//                .queryParam("pagesize", "3")
+//                .build().toUriString();
+//        log.info("* testGetAllContacts, webTestClient={}\n", webTestClient);
+
+        EntityExchangeResult<List<Contact>> response = webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/contact")
+                        .queryParam("page", "0")
+                        .queryParam("pagesize", "3")
+                        .build()) // the base URL is already configured for us   "/contact?page=0&pagesize=3"
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body = response.getResponseBody();
+        log.info("* GetAllContactsWithParamTest, List<Contact>=\n{}", body);
+        assertThat(body).isNotNull();
+        assertThat(body.size()).isEqualTo(3);
+    }
+
+    @Test
+    void findAllContactsMatchingPatternTest() throws JsonProcessingException {
+        Contact contact = new Contact(null, "john", null, null, null, null);
+        EntityExchangeResult<List<Contact>> response = webTestClient
+                .post()
+                .uri("/contact/find")
+                .body(Mono.just(contact), Contact.class)
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body = response.getResponseBody();
+        log.info("* findAllContactsMatchingPatternTest c1, List<Contact>=\n{}", body);
+        assertThat(body).isNotNull();
+        assertThat(body.size()).isEqualTo(2);
+
+        Contact contact2 = new Contact(1L, null, null, null, null, null);
+        EntityExchangeResult<List<Contact>> response2 = webTestClient
+                .post()
+                .uri("/contact/find")
+                .body(Mono.just(contact2), Contact.class)
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body2 = response2.getResponseBody();
+        log.info("* findAllContactsMatchingPatternTest c2, List<Contact>=\n{}", body2);
+        assertThat(body2).isNotNull();
+        assertThat(body2.size()).isEqualTo(1);
+    }
+
+    @Test
+    void CreateUpdateDeleteNewContactTest() {
+        Contact contact = new Contact(null, "a", "b", "5", "aa@a.a", "c");
+
+        //add contact
+        EntityExchangeResult<List<Contact>> response = webTestClient
+                .post()
+                .uri("/contact/add")
+                .body(Mono.just(contact), Contact.class)
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body = response.getResponseBody();
+        log.info("* CreateUpdateDeleteNewContactTest add, Contact=\n{}", body);
+        assertThat(body).isNotNull();
+        assertThat(body.size()).isEqualTo(1);
+        assertThat(body.get(0).getId()).isPositive();
+
+        //update contact
+        contact.setId(body.get(0).getId());
+        contact.setFirstName("aa");
+        EntityExchangeResult<List<Contact>> response2 = webTestClient
+                .put()
+                .uri("/contact/update")
+                .body(Mono.just(contact), Contact.class)
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body2 = response2.getResponseBody();
+        log.info("* CreateUpdateDeleteNewContactTest update, Contact=\n{}", body2);
+        assertThat(body2).isNotNull();
+        assertThat(body2.size()).isEqualTo(1);
+        assertThat(body2.get(0).getFirstName()).isEqualTo("a");
+
+        //delete contact
+        EntityExchangeResult<List<Contact>> response3 = webTestClient
+                .method(HttpMethod.DELETE)
+                .uri("/contact/delete")
+                .body(Mono.just(contact), Contact.class)
+                .accept(APPLICATION_JSON, APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Contact.class)
+                .returnResult();
+        List<Contact> body3 = response3.getResponseBody();
+        log.info("* CreateUpdateDeleteNewContactTest update, Contact=\n{}", body3);
+        assertThat(body3).isNotNull();
+        assertThat(body3.size()).isEqualTo(1);
+        assertThat(body3.get(0).getId()).isEqualTo(contact.getId());
+    }
+
+}
+
 //		String json = this.objectMapper.writeValueAsString(new Contact(1L, "", "", "", "", ""));
 
-        this.webTestClient
-                .get()
-                .uri("http://localhost:8080/contact")
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .exchange()
-                .expectStatus()
-                .is2xxSuccessful()
-                .expectHeader()
-                .contentType(APPLICATION_JSON)
-                .expectBody()
-                .jsonPath("$.length()").isEqualTo(3)
-                .jsonPath("$[0].id").isEqualTo(1)
-                .jsonPath("$[0].name").isEqualTo("duke")
-                .jsonPath("$[0].tags").isNotEmpty();
-    }
+//        this.webTestClient
+//                .get()
+//                .uri("http://localhost:8080/contact")
+//                .header(ACCEPT, APPLICATION_JSON_VALUE)
+//                .exchange()
+//                .expectStatus()
+//                .is2xxSuccessful()
+//                .expectHeader()
+//                .contentType(APPLICATION_JSON)
+//                .expectBody()
+//                .jsonPath("$.length()").isEqualTo(3)
+//                .jsonPath("$[0].id").isEqualTo(1)
+//                .jsonPath("$[0].name").isEqualTo("duke")
+//                .jsonPath("$[0].tags").isNotEmpty();
+//        webClient.get();
 
-    @Disabled
-    @Test
-    void shouldCreateNewUser() {
-
-        Contact contact = new Contact(1L, "", "", "", "", "");
-
-        FluxExchangeResult<Void> userCreationResponse = this.webTestClient
-                .post()
-                .uri("/api/users")
-                .body(Mono.just(contact), Contact.class)
-                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .exchange()
-                .expectStatus()
-                .isEqualTo(CREATED)
-                .returnResult(Void.class);
-
-        String locationUrlOfNewUser = userCreationResponse.getResponseHeaders().get(LOCATION).get(0);
-
-        this.webTestClient
-                .get()
-                .uri(locationUrlOfNewUser)
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .exchange()
-                .expectStatus()
-                .isEqualTo(OK)
-                .expectBody(Contact.class)
-                .isEqualTo(contact);
-
-        this.webTestClient
-                .delete()
-                .uri("/api/users/{id}", contact.getId())
-                .exchange();
-    }
-}
+//        FluxExchangeResult<Void> userCreationResponse = this.webTestClient
+//                .post()
+//                .uri("/api/users")
+//                .body(Mono.just(contact), Contact.class)
+//                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+//                .header(ACCEPT, APPLICATION_JSON_VALUE)
+//                .exchange()
+//                .expectStatus()
+//                .isEqualTo(CREATED)
+//                .returnResult(Void.class);
+//
+//        String locationUrlOfNewUser = userCreationResponse.getResponseHeaders().get(LOCATION).get(0);
+//
+//        this.webTestClient
+//                .get()
+//                .uri(locationUrlOfNewUser)
+//                .header(ACCEPT, APPLICATION_JSON_VALUE)
+//                .exchange()
+//                .expectStatus()
+//                .isEqualTo(OK)
+//                .expectBody(Contact.class)
+//                .isEqualTo(contact);
+//
+//        this.webTestClient
+//                .delete()
+//                .uri("/api/users/{id}", contact.getId())
+//                .exchange();
--- src/test/java/com/example/ContactManager/Controller/ContactControllerTest.java
+++ src/test/java/com/example/ContactManager/Controller/ContactControllerTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class ContactControllerTest {
+
+
+
+
     @Test
     void getContacts() {
     }
